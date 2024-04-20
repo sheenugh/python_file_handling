346b8e3ce9c1ceeeb75c89dd219fbea84c8476a5
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-04-20 18:09:24 +0800

    PSEUDO CODE

---
+++ read_file.py
@@ -0,0 +1,16 @@
+# Delima, Sheena Mae Delima
+# BSCPE 1-2
+# April 20, 2024
+
+# --------------------------------------------
+
+# ========== PSEUDO CODE ===========
+# || LIBRARIES \ PACKAGES
+
+# || VARIABLES || 
+
+# || FUNCTIONS
+
+# || ACTUAL CODES ||
+# - Opening the file.
+# - For loop code
