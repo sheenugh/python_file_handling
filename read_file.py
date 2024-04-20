328a391cee853bc5dee55a6b1d61693bfcd80093
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-04-20 19:03:31 +0800

    Adding eve num csv file and code for t it

--- read_file.py
+++ read_file.py
@@ -10,17 +10,16 @@
 # || IMPORTS || 
 import csv
 
-# || VARIABLES || 
-
-# || FUNCTIONS
-
 # || ACTUAL CODES ||
+# - FOR ODD NUMBERS
 # - Opening the file.
 with open('positive_integers.csv', 'r') as file:
     reader = csv.reader(file)
     
-    next(reader)
+    # - Removing the header while reading the file.
+    next(reader) 
     
+    # - Creating a csv file for the output.
     with open('odd_numbers.csv', 'w', newline="") as output_file:
         writer = csv.writer(output_file)
         
@@ -30,5 +29,24 @@ with open('positive_integers.csv', 'r') as file:
         
         for line in reader:
             writer.writerow([line[0]])
+            
+# - FOR EVEN NUMBERS
+# - Opening the file.
+with open('positive_integers.csv', 'r') as file:
+    reader = csv.reader(file)
+    
+    # - Removing the header while reading the file.
+    next(reader) 
+    
+    # - Creating a csv file for the output.
+    with open('even_numbers.csv', 'w', newline="") as output_file:
+        writer = csv.writer(output_file)
         
-# - For loop code
+        header = ['Even Numbers']
+        
+        writer.writerow(header)
+        
+        for line in reader:
+            writer.writerow([line[1]])
+        
+        
