9aba0ef3005733205a033e46a1101b3286618dea
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-04-20 18:51:27 +0800

    Adding code for making a .csv file.

--- read_file.py
+++ read_file.py
@@ -5,7 +5,10 @@
 # --------------------------------------------
 
 # ========== PSEUDO CODE ===========
-# || LIBRARIES \ PACKAGES
+# || LIBRARIES \ PACKAGES ||
+
+# || IMPORTS || 
+import csv
 
 # || VARIABLES || 
 
@@ -13,4 +16,13 @@
 
 # || ACTUAL CODES ||
 # - Opening the file.
+with open('python_file_handling\positive_integers.csv', 'r') as file:
+    reader = csv.reader(file)
+    
+    next(reader)
+    
+    with open('odd_numbers.csv', 'w') as output_file:
+        writer = csv.writer(output_file)
+        
+        
 # - For loop code
