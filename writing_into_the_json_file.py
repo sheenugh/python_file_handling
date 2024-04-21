aee971d856d8a42662ac241f5b2dc7ba411f90b4
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-04-21 09:07:00 +0800

    So far gumagana na ang pag input gamit ang technique itong writing into sa magagawang jon file.

--- writing_into_the_json_file.py
+++ writing_into_the_json_file.py
@@ -6,9 +6,24 @@
 
 # ========== PSEUDO CODE ===========
 # || LIBRARIES \ PACKAGES ||
+import json
+
 
 # || VARIABLES ||
+user_name = input("Enter your name:")
+user_age = int(input("Enter your age:"))
+user_fav_food = input("Enter your favorite food:")
+user_fav_color = input("Enter your favorite color:")
+user_role_model = input("Enter your role model/s:")
+user_tutor_in_programming = input("Enter who taught you this file handling so early:")
+
 
 # || ACTUAL CODE ||
 # - Stored data of various questions.
+
+life_data = {
+    "name" : user_name
+    
+}
+
 # - Code for creating a new JSON file
