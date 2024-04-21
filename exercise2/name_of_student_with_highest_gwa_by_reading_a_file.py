588558af412a3cc3f76a1b5719b3cd485637cc55
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-04-21 14:12:47 +0800

    Reading the rows and finding the student with highest gwa.

--- exercise2/name_of_student_with_highest_gwa_by_reading_a_file.py
+++ exercise2/name_of_student_with_highest_gwa_by_reading_a_file.py
@@ -18,5 +18,12 @@ max_student = None
 # - Open and reading the csv file.
 with open('python_file_handling\exercise2\students_grade_info.csv', 'r') as file:
     reader = csv.reader(file)
+    
 # - Reading the rows and find the student who has a highest GWA.
+    for row in reader:
+        name, gwa = row[0], float(row[1])
+        if gwa <= max_gwa:
+            max_gwa = gwa
+            max_student = name
+
 # - Printing the output containing the student name and his/her highest GWA.
