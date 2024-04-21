cd5575d6eb850acef92a9391154d9d0d2a4948c1
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-04-21 12:16:31 +0800

    Nagawa naman kaso my changes pa.

--- exercise4/reading_and_writing_a_two_file_squared_and_cubed_file.py
+++ exercise4/reading_and_writing_a_two_file_squared_and_cubed_file.py
@@ -14,13 +14,31 @@ import csv
 
 # || ACTUAL CODES ||
 # - Opening the file and reading the file.
-with open("C:/Users/Myline/Desktop/OOP/Assignment 4/python_file_handling/exercise4/positive_integers_file.csv", "r") as file:
-    reader = csv.reader(file)
+def process_integers():
+    with open("positive_integers_file.cs", "r") as file:
+        reader = csv.reader(file)
+    
+    # Loop through the rows in the CSV file
+        integers = [int(row[0]) for row in reader[:20]]
 
 # - Writing a file: squared_integers and cubed_integers.
     with open("triple.csv", "w", newline="") as output1, open("doubled.csv", "w", newline="") as output2:
         tripled_file = csv.writer(output1)
         doubled_file = csv.writer(output2)
+        
+# Loop through the integers
+        for num in integers:
+            # Check if the number is even
+            if num % 2 == 0:
+                # Write the square of the number to the double.csv file
+                doubled_file.writerow([num ** 2])
+            else:
+                # Write the cube of the number to the triple.csv file
+                tripled_file.writerow([num ** 3])v
+
+# Call the function
+process_integers()
+        
         
 
         
