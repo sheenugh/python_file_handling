4568c1db7a1028d89c044f24d3be9497bcdad05b
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-04-21 13:02:41 +0800

    All done: Result for squared and cubed numbers from integers.txt. Done also the codes in main py file.

--- exercise4/reading_and_writing_a_two_file_squared_and_cubed_file.py
+++ exercise4/reading_and_writing_a_two_file_squared_and_cubed_file.py
@@ -5,31 +5,23 @@
 # ------------------------------------------------------
 
 # ========= PSEUDO CODE =========
-# || LIBRARIES / PACKAGES ||
-import csv
-
-# || VARIABLES || 
-
-# || FUNCTIONS ||
-
 # || ACTUAL CODES ||
 # - Opening the file and reading the file.
-def process_integers():
+def squared_and_cubed_numbers_from_src():
     with open("integers.txt", "r") as file:
         integers = [int(line.strip()) for line in file.readlines()[:20]]
     
 # - Writing a file: double_integers and triple_integers.
-with open("double.txt", "w") as output1, open("triple.txt", "w") as output2:
-    for num in integers:
-        
-        
-
-
-
+    with open("double.txt", "w") as output1, open("triple.txt", "w") as output2:
+        for num in integers:
+            if num % 2 == 0:
+                output1.write(f'{num * num}\n')
+        else:
+                # Write the cube of the number to the triple.txt file
+                output2.write(f'{num * num * num}\n')
 
 # Call the function
-process_integers()
-        
+squared_and_cubed_numbers_from_src()
         
 
         
