8479f370f72e118c4dcf4919d69dc80d81f738dd
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-04-21 13:48:53 +0800

    PSEUDO CODE

---
+++ exercise2/name_of_student_with_highest_gwa_by_reading_a_file.py
@@ -0,0 +1,15 @@
+# Delima, Sheena Mae Delima
+# BSCPE 1-2
+# April 21, 2024
+
+# --------------------------------------------
+
+# ========== PSEUDO CODE ===========
+# || LIBRARIES \ PACKAGES ||
+
+# || VARIABLES ||
+
+# || ACTUAL CODES ||
+# - Open and reading the csv file.
+# - Reading the rows and find the student who has a highest GWA.
+# - Printing the output containing the student name and his/her highest GWA.
