14851bc3f98c4dfe298e8685bd49c4468983294a
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-04-21 08:53:06 +0800

    PSEUDO CODE in writing into the json fike

---
+++ writing_into_the_json_file.py
@@ -0,0 +1,14 @@
+# Delima, Sheena Mae Delima
+# BSCPE 1-2
+# April 20, 2024
+
+# --------------------------------------------
+
+# ========== PSEUDO CODE ===========
+# || LIBRARIES \ PACKAGES ||
+
+# || VARIABLES ||
+
+# || ACTUAL CODE ||
+# - Stored data of various questions.
+# - Code for creating a new JSON file
